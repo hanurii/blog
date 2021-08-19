c6085f3cbde5064df9605a3f19f34f489ce882ee
최한울 <han95210@naver.com>
2021-08-19 23:52:01 +0900

    Update gatsby-meta-config.js

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  title: 'Dev Ed', // Your website title
-  description: `Ed's Blog`, // Your website description
-  author: 'Ed', // Maybe your name
-  siteUrl: 'https://gatsby-starter-apple.netlify.app', // Your website URL
-  lang: 'en', // Language
-  utterances: 'sungik-choi/gatsby-starter-apple-comment', // Github repository to store comments
+  title: 'hanuri', // Your website title
+  description: `hanuri's Blog`, // Your website description
+  author: 'HANUL CHOI', // Maybe your name
+  siteUrl: 'https://hanuri.netlify.app/', // Your website URL
+  lang: 'ko', // Language
+  utterances: '', // Github repository to store comments
   links: {
-    github: 'https://github.com/sungik-choi/gatsby-starter-apple', // Your github repository
+    github: 'https://github.com/Hamill210', // Your github repository
   },
   icon: 'src/images/icon.png', //  Favicon Path
 };
